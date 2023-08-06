e5f6f5759124a0bb37f54fc0a1d4081a12e0c2e5
alexandrarozanova <139215437+alexandrarozanova@users.noreply.github.com>
2023-08-06 17:00:53 +0700

    Delete AreaComparator.java

--- Shapes/src/ru/academits/rozanova/shapes/AreaComparator.java
+++
@@ -1,9 +0,0 @@
-package ru.academits.rozanova.shapes;
-
-import java.util.Comparator;
-
-public class AreaComparator implements Comparator<Shape> {
-    public int compare(Shape shape1, Shape shape2) {
-        return Double.compare(shape1.getArea(), shape2.getArea());
-    }
-}
