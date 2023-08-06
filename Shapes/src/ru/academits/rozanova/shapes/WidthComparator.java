8eee1acc20b0f4c17f706e71fc37b34a139d4194
alexandrarozanova <139215437+alexandrarozanova@users.noreply.github.com>
2023-08-06 17:01:27 +0700

    Delete WidthComparator.java

--- Shapes/src/ru/academits/rozanova/shapes/WidthComparator.java
+++
@@ -1,9 +0,0 @@
-package ru.academits.rozanova.shapes;
-
-import java.util.Comparator;
-
-public class WidthComparator implements Comparator<Shape> {
-    public int compare(Shape shape1, Shape shape2) {
-        return Double.compare(shape1.getWidth(), shape2.getWidth());
-    }
-}
