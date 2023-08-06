c627c7ad0bc23de7178efdd1399cccd3582e4b5f
alexandrarozanova <139215437+alexandrarozanova@users.noreply.github.com>
2023-08-06 17:01:09 +0700

    Delete HeightComparator.java

--- Shapes/src/ru/academits/rozanova/shapes/HeightComparator.java
+++
@@ -1,9 +0,0 @@
-package ru.academits.rozanova.shapes;
-
-import java.util.Comparator;
-
-public class HeightComparator implements Comparator<Shape> {
-    public int compare(Shape shape1, Shape shape2) {
-        return Double.compare(shape1.getHeight(), shape2.getHeight());
-    }
-}
